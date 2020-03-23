efdc0fb92c928ef3f81528558684574bd60cd0b3
mxrcx <m-schaarschmidt@outlook.de>
2020-03-22 22:12:59 -0700

    Added FightCow Task

--- src/FightCow.java
+++ src/FightCow.java
@@ -1,3 +1,6 @@
+import org.rspeer.runetek.adapter.scene.Npc;
+import org.rspeer.runetek.api.scene.Npcs;
+import org.rspeer.runetek.api.scene.Players;
 import org.rspeer.script.task.Task;
 
 /**
@@ -6,11 +9,17 @@ import org.rspeer.script.task.Task;
 public class FightCow extends Task {
     @Override
     public boolean validate() {
-        return false;
+        return Npcs.getNearest(nearest -> nearest.isPositionInteractable() && nearest.getName().equals("Cow")) != null;
     }
 
     @Override
     public int execute() {
+        Npc cow = Npcs.getNearest(nearest -> nearest.isPositionInteractable() && nearest.getName().equals("Cow"));
+
+        while (cow.getHealthPercent() > 0) {
+            cow.interact("Attack");
+        }
+
         return 0;
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,11 @@
+import org.rspeer.script.ScriptCategory;
 import org.rspeer.script.ScriptMeta;
 import org.rspeer.script.task.TaskScript;
 
-@ScriptMeta(name = "Test-Bot", developer = "Marco Schaarschmidt", desc = "This is a Test-Bot.")
+@ScriptMeta(name = "Test-Bot", developer = "Marco Schaarschmidt", desc = "This is a Test-Bot.", category = ScriptCategory.COMBAT)
 public class Test extends TaskScript {
     @Override
     public void onStart() {
-        submit();
+        submit(new FightCow(), new Pickup(), new Restore());
     }
 }
