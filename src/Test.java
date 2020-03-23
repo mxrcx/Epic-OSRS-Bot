922df9ac30ba588bfeb330bb9ede87fee1c6d26e
David-Rodden <davidrodden273@gmail.com>
2020-03-22 20:42:36 -0700

    simple pickup test

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,9 @@
+import org.rspeer.runetek.adapter.Interactable;
+import org.rspeer.runetek.adapter.Positionable;
+import org.rspeer.runetek.api.commons.Time;
+import org.rspeer.runetek.api.commons.math.Random;
+import org.rspeer.runetek.api.component.tab.Inventory;
+import org.rspeer.runetek.api.scene.Pickables;
 import org.rspeer.script.Script;
 import org.rspeer.script.ScriptMeta;
 
@@ -5,11 +11,16 @@ import org.rspeer.script.ScriptMeta;
 public class Test extends Script {
     @Override
     public void onStart() {
+        // soethign
         //testing phase
     }
 
     @Override
     public int loop() {
-        return 0;
+        final Interactable toPick = Pickables.getNearest(Positionable::isPositionInteractable);
+        toPick.interact("Take");
+        final int freeSpaces = Inventory.getFreeSlots();
+        Time.sleepUntil(() -> Inventory.getFreeSlots() != freeSpaces, Random.mid(1000, 2000));
+        return Random.mid(800, 1000);
     }
 }
