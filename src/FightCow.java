9413a719f6bf44967a3745ce7e31378535439ab9
mxrcx <m-schaarschmidt@outlook.de>
2020-03-22 22:50:45 -0700

    Added Pickup

--- src/FightCow.java
+++ src/FightCow.java
@@ -1,4 +1,6 @@
+import org.rspeer.runetek.adapter.Interactable;
 import org.rspeer.runetek.adapter.scene.Npc;
+import org.rspeer.runetek.api.commons.Time;
 import org.rspeer.runetek.api.scene.Npcs;
 import org.rspeer.runetek.api.scene.Players;
 import org.rspeer.script.task.Task;
@@ -9,16 +11,16 @@ import org.rspeer.script.task.Task;
 public class FightCow extends Task {
     @Override
     public boolean validate() {
-        return Npcs.getNearest(nearest -> nearest.isPositionInteractable() && nearest.getName().equals("Cow")) != null;
+        return Npcs.getNearest(nearest -> nearest.isPositionInteractable() && nearest.getName().equals("Cow") && nearest.getTarget() == null) != null && Players.getLocal().getTarget() == null;
     }
 
     @Override
     public int execute() {
-        Npc cow = Npcs.getNearest(nearest -> nearest.isPositionInteractable() && nearest.getName().equals("Cow"));
+        final Interactable cow = Npcs.getNearest(nearest -> nearest.isPositionInteractable() && nearest.getName().equals("Cow"));
 
-        while (cow.getHealthPercent() > 0) {
-            cow.interact("Attack");
-        }
+        cow.interact("Attack");
+
+        Time.sleepUntil(() -> Players.getLocal().getTarget().getHealthPercent() > 0, 2000);
 
         return 0;
     }
--- src/Pickup.java
+++ src/Pickup.java
@@ -1,13 +1,21 @@
+import org.rspeer.runetek.adapter.scene.Pickable;
+import org.rspeer.runetek.api.scene.Pickables;
+import org.rspeer.runetek.api.scene.Players;
 import org.rspeer.script.task.Task;
 
 public class Pickup extends Task {
     @Override
     public boolean validate() {
-        return false;
+
+        return Pickables.getNearest(nearest -> nearest.isPositionInteractable() && nearest.getName().equals("Cowhide") || nearest.getName().equals("Raw beef") || nearest.getName().equals("Bones")) != null;
     }
 
     @Override
     public int execute() {
+        Pickable item = Pickables.getNearest(nearest -> nearest.isPositionInteractable() && nearest.getName().equals("Cowhide") || nearest.getName().equals("Raw beef") || nearest.getName().equals("Bones"));
+
+        item.interact("Pick up");
+
         return 0;
     }
 }
